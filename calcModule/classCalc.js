ebf6a244fc1d69549c312f814f1feefc2a4358ce
Baruch Guerra <dolce_dmn@hotmail.com>
2019-08-29 16:39:03 -0500

    beautify code

--- calcModule/classCalc.js
+++ calcModule/classCalc.js
@@ -1,30 +1,19 @@
 export class Calculator  {
     constructor (){
-        this.add = function(a,b) {
-            return a+b;
-        }
-        this.substraction = function(a, b) {
-            return a-b;
-        };
-        this.multiplication = function(a, b) { 
-            return a*b;
-        };
-        this.division = function(a, b) { 
-            return a/b; 
-        };
+        this.add = (a,b) => a+b;
+        this.substraction = (a, b)=> a-b;
+        this.multiplication = (a, b) => a*b;
+        this.division = (a, b) => a/b; 
     };
     adition(a, b){
         return this.add(a, b);
     }
-
     subtract(a, b){
         return this.substraction(a, b);
     }
-
     multiply(a, b){
         return this.multiplication(a, b);
     }
-
     divide(a, b){
         return this.division(a, b);
     }
--- calcModule/importingCalcClass.js
+++ calcModule/importingCalcClass.js
@@ -6,7 +6,7 @@ console.log("Soy de la clase calculadora");
 console.log(calc.adition(4, 5));
 console.log(calc.subtract(4, 5));
 console.log(calc.multiply(4, 5));
-console.log(calc.divide(4, 5));
+console.log(calc.divide(5, 5));
 
 
 
